cde2eef60502da0633e7c1f8cde8e8ec1216e9c2
YILMAZ KAYALI <yilmazanilkayali@gmail.com>
2022-03-25 17:58:46 +0300

    Unit Of Work Oluturuldu.

---
+++ OrderApplication.Data/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApplication.Data.Repository.IRepository
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        //IDiposable, iunitofworkun işlemi bittiği zaman kendisi otomatik olarak silinmesini sağlar.
+        //Tüm Repositoryler burada toplanır, toplu olarak save changes() yapılır.
+
+        IAppUserRepository AppUser { get; }
+        ICartRepository Cart { get; }
+        ICategoryRepository Category { get; }
+        IOrderDetailsRepository OrderDetails { get; }
+        IOrderProductRepository OrderProduct { get; }
+        IProductRepository Product { get; }
+
+        void Save();
+
+
+
+
+
+    }
+}
---
+++ OrderApplication.Data/Repository/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using OrderApplication.Data.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApplication.Data.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        //UOW:Tüm instance işlemlerinin tek bir yerden tek instance oluşturularak kullanılmasına olanak sağlar.
+
+        private ApplicationDbContext _context;
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IAppUserRepository AppUser => new AppUserRepository(_context);
+
+        public ICartRepository Cart => new CartRepository(_context);
+
+        public ICategoryRepository Category => new CategoryRepository(_context);
+
+        public IOrderDetailsRepository OrderDetails => new OrderDetailsRepository(_context);
+
+        public IOrderProductRepository OrderProduct => new OrderProductRepository(_context);
+
+        public IProductRepository Product => new ProductRepository(_context);
+
+     
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+        public void Dispose()
+        {
+            //save işlemi bittikten sonra işlemin ramde gerksiz tutulmasını önlemek amacıylaa kullanılır. tamamlanan işlevi ramden kaldırır. performansa yöneliktir.
+            _context.Dispose();
+        }
+    }
+}
