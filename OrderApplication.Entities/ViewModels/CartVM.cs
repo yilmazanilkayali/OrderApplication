590ca41637e5098e940f61d06e6fc996789eb5fb
YILMAZ KAYALI <yilmazanilkayali@gmail.com>
2022-03-30 18:17:19 +0300

    CartController and CartVM created

---
+++ OrderApplication.Entities/ViewModels/CartVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApplication.Entities.ViewModels
+{
+    public class CartVM
+    {
+        public OrderProduct OrderProduct { get; set; }
+        public IEnumerable<Cart> CartList { get; set; }
+
+    }
+}
---
+++ OrderApplication/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderApplication.Data.Repository.IRepository;
+
+namespace OrderApplication.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
