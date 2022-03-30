561c819a1eeb8b42d3186f74ee1247aba3a4d575
YILMAZ KAYALI <yilmazanilkayali@gmail.com>
2022-03-30 18:41:37 +0300

    index in cartcontroller is filled. Created index View

--- OrderApplication/Areas/Customer/Controllers/CartController.cs
+++ OrderApplication/Areas/Customer/Controllers/CartController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OrderApplication.Data.Repository.IRepository;
+using OrderApplication.Entities.ViewModels;
+using System.Security.Claims;
 
 namespace OrderApplication.Areas.Customer.Controllers
 {
@@ -7,13 +9,29 @@ namespace OrderApplication.Areas.Customer.Controllers
     public class CartController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        public CartVM CartVM { get; set; }
         public CartController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
+        //Sisteme giriş yapan kullanıcının sepetteki ürünlerini listeler.
         public IActionResult Index()
         {
-            return View();
+             var claimsIdetity = (ClaimsIdentity)User.Identity;
+            var claim =claimsIdetity.FindFirst(ClaimTypes.NameIdentifier);
+
+            CartVM = new CartVM()
+            {
+                CartList = _unitOfWork.Cart.GetAll(p => p.AppUserId == claim.Value,includeProperties:"Product"),
+                OrderProduct = new()
+            };
+            foreach (var cart in CartVM.CartList)
+            {
+                cart.Price = cart.Product.Price * cart.Count; 
+                CartVM.OrderProduct.OrderPrice += cart.Price ;
+
+            }
+            return View(CartVM);
         }
     }
 }
