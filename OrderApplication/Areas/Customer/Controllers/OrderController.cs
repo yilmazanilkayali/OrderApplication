51a968481bc0c5405f743b5e675453c80cf39151
YILMAZ KAYALI <yilmazanilkayali@gmail.com>
2022-04-01 17:25:30 +0300

    Created OrderController

---
+++ OrderApplication/Areas/Customer/Controllers/OrderController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderApplication.Data.Repository.IRepository;
+
+namespace OrderApplication.Areas.Customer.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
